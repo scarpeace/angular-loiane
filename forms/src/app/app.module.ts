32ca6bddad0487aa1ce3a42f90120801dc0b4be2
scarpeace <gustavo.scarpellini@gmail.com>
2019-11-28 17:54:51 -0300

    Ultimo Commit antes da Refatoracao de Codigo

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -6,7 +6,7 @@ import {FormsModule} from "@angular/forms"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataFormComponent } from './components/data-form/data-form.component';
-import { TemplateFormModule } from './template-form/template-form.module';
+import { TemplateFormModule } from './components/template-form/template-form.module';
 
 @NgModule({
   declarations: [
--- forms/src/app/components/template-form/template-form.module.ts
+++ forms/src/app/components/template-form/template-form.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { TemplateFormComponent } from '../components/template-form/template-form.component';
+import { FormDebugComponent } from '../form-debug/form-debug.component';
+import { TemplateFormComponent } from './template-form.component';
+
 
 
 
 @NgModule({
-  declarations: [TemplateFormComponent],
+  declarations:[FormDebugComponent, TemplateFormComponent],
   imports: [
     CommonModule,
     FormsModule,
